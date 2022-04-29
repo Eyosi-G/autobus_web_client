09119d6decda73f912ae0a7609023269e3bd4c95
= <eyosiyasgirma0@gmail.com>
2022-04-29 07:33:44 +0300

    production link added

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:8080";
+export const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://autobus.herokuapp.com"
+    : "http://localhost:8080";
 
 const createInstance = () => {
   const data = localStorage.getItem("data");
-  console.log(data, "ceate instance")
+  console.log(data, "ceate instance");
   if (data && JSON.parse(data).token) {
     return axios.create({
       baseURL: `${baseURL}/api/v1`,
